bcb623a1c06783cfbf1edaa178c0e6ddaa9caa6a
Agustin Gamio <agamio@urudatasoftware.com>
2025-07-27 11:57:51 -0300

    refactor: day_type

--- HPCCImp/day_type.c
+++ HPCCImp/day_type.c
@@ -1,34 +1,22 @@
 #include "day_type.h"
 #include <time.h>
 
-DayType get_previous_day_type(DayType day){
-    switch (day) {
-        case WORKING_DAY:
-            return SUNDAY; //TODO: Revisar si no es mejor devolver WORKING_DAY
-        case SATURDAY:
-            return WORKING_DAY;
-        case SUNDAY:
-            return SATURDAY;
-        default:
-            return WORKING_DAY;
-    }
+DayType get_previous_day_type(const DayType day){
+    if (day == SUNDAY)
+        return SATURDAY;
+
+    return WORKING_DAY;
 }
 
-DayType get_next_day_type(DayType day){
-    switch (day) {
-        case WORKING_DAY:
-            return SATURDAY; //TODO: Revisar si no es mejor devolver WORKING_DAY
-        case SATURDAY:
-            return SUNDAY;
-        case SUNDAY:
-            return WORKING_DAY;
-        default:
-            return WORKING_DAY;
-    }
+DayType get_next_day_type(const DayType day){
+    if (day == SATURDAY)
+        return SUNDAY;
+
+    return WORKING_DAY;
 }
 
-DayType get_day_type_from_date(struct tm date) {
-    int day_of_week = date.tm_wday;
+DayType get_day_type_from_date(const struct tm date) {
+    const int day_of_week = date.tm_wday;
     
     switch (day_of_week) {
         case 0:
